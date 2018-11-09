bc8f735b05659065824b7042309d690bc0b9fb14
Nazaretedesco <42558411+Nazaretedesco@users.noreply.github.com>
2018-11-09 00:07:12 -0200

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,23 @@
+/* P3.c*/
+/* Adrielle */
+/*11811EBI005*/
+
+#include <stdio.h>
+
+int main(){
+	int i,x=0;
+	char num[256];
+	printf("\nInsira um numero:");
+	scanf("%s",num);
+	printf("Apenas os numeros:");
+	for(i=0;num[i]!='\0'; i++){
+       if(num[i] >='0' && num[i] <='9' ){
+       	printf("%d",num[i]-'0');
+       	x++;
+       }
+	}
+	if(x==0){
+	printf("0");	
+	}
+	return 0;
+    }
